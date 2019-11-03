acf784b337866f9e1171cae2064b3906190d20c2
osvanicolina <osvacolijim@gmail.com>
2019-11-03 11:47:10 -0400

    Refactor project

--- config/yargs.js
+++ config/yargs.js
@@ -1,29 +1,26 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+
 const argv = require('yargs')
     .command('crear', 'Crear un elemento por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
     .command('actualizar', 'Actualiza el estado completado de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        },
-        completado: {
-            alias: 'c',
-            default: true,
-            desc: 'Marca como completado o pendiente la tarea'
-        }
+        descripcion,
+        completado
     })
     .command('borrar', 'Borra una tarea de la lista de tareas', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea por hacer'
-        }
+        descripcion
     })
     .help()
     .argv;
